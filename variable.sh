72e5121ac40c8073354a7b553b9b68de64699fa9
abanoth <ajaybanoth.aj@gmail.com>
2025-12-23 19:10:37 -0600

    changed the name

---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=ajay
+PERSON2=hameed
+
+echo "$PERSON1: hello friend how are you?"
+echo "$PERSON2: i am fine thanks for asking"
+echo "$PERSON1: great to hear that! what are you up to today?"
+echo "$PERSON2: i am just relaxing at the moment, how about you?"
+echo "$PERSON1: i am just working on some projects, but i am also trying to stay active and healthy"
+echo "$PERSON2: that sounds like a good balance! i should try to do more of that myself"
