5e8be7a80ddae19b6d3e9af6467479cce06b0fbe
abanoth <ajaybanoth.aj@gmail.com>
2025-12-23 22:52:06 -0600

    pin number defined

--- 5-variable.sh
+++ 5-variable.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 echo "Please enter you PIN Number"
-read PIN_NUMBER  # Here pin number is the variable name which will store the input value
+read  -s PIN_NUMBER  # Here pin number is the variable name which will store the input value
+# -s will hide the input value
 
 echo " PIN entered is: $PIN_NUMBER"
