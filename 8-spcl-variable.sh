400bb5bfb5e227c630efded5d8dab9ceb8ae142d
abanoth <ajaybanoth.aj@gmail.com>
2025-12-23 23:56:33 -0600

    directory deatils

--- 8-spcl-variable.sh
+++ 8-spcl-variable.sh
@@ -3,6 +3,6 @@
 echo " All variables passed to the script: $@"
 echo " All variables passed to the script: $*"
 echo " Script name: $0"
-echo " what is the current working directory: $pwd"
+echo " what is the current working directory: $PWD"
 echo " who is running this script: $USER"
 echo " what is the home directory of the user running this script: $HOME"
