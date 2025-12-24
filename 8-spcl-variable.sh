c8a8d38609c7849ed6613bbf6e7f5ceb8fb605f2
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 00:04:24 -0600

    id details

--- 8-spcl-variable.sh
+++ 8-spcl-variable.sh
@@ -7,3 +7,7 @@ echo " what is the current working directory: $PWD"
 echo " who is running this script: $USER"
 echo " what is the home directory of the user running this script: $HOME"
 echo " PID of the script: $$"
+# useful to chekc the PID of the script
+sleep 50 &
+echo " PID of the las command run in the background: $!" 
+# useful to chekc the PID of the last command run in the background
