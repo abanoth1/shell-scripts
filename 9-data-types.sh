0095390b835145c2fbedc24ac7770eda7cc2ae3a
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 00:38:30 -0600

    data type modified

--- 9-data-types.sh
+++ 9-data-types.sh
@@ -9,6 +9,12 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is : ${SUM}"
 
+# ARRAY IN BASH SCRIPT
+# SYNTAX : ARRAY_NAME=(VALUE1 VALUE2 VALUE3)
+# Size of the array = 4, index starts from 0 i,e 3
+
 LEADERS=("Rahul" "Sachin" "Dhoni" "Kohli")
 
 echo "All the leaders are : ${LEADERS[@]}"
+echo "First Leader is : ${LEADERS[0]}"
+echo "Second Leader is : ${LEADERS[1]}"
