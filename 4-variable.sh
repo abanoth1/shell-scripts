09d5ef6d5c5fbafdf309c437b895c1678ff8fe79
abanoth <ajaybanoth.aj@gmail.com>
2025-12-23 22:30:39 -0600

    new change

---
+++ 4-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Argumnets are extra information you give to your script from the terminal.
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Hello, $PERSON1! How are you today?"
+echo "Hello, $PERSON2! How are you today?"
+echo "$PERSON1 i'm doing great, thanks for asking."
+echo "$PERSON2 i'm doing great, thanks for asking."
+echo "$PERSON1  i am a software developer."
+echo "$PERSON2  i am a devops engineer."
