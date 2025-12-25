50bc5c4cf50b403e964331530fdf6ea44f64fc87
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 19:45:09 -0600

    all conditions added

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -11,8 +11,8 @@ if [ "$NUMBER" -lt 10 ]; then   # -lt is less than operator
     echo "The given number $NUMBER is equal to 10."
     elif [ "$NUMBER" -gt 10 ]; then   # -gt is greater than operator
     echo "The given number $NUMBER is greater than 10."
-    elif [ "$NUMBER" -le 10 ]; then   # -le is less than or equal to operator
-    echo "The given number $NUMBER is less than or equal to 10."
+    #elif [ "$NUMBER" -le 10 ]; then   # -le is less than or equal to operator
+    #echo "The given number $NUMBER is less than or equal to 10."
     elif [ "$NUMBER" -ge 10 ]; then   # -ge is greater than or equal to operator
     echo "The given number $NUMBER is greater than or equal to 10."
     elif [ "$NUMBER" -ne 10 ]; then   # -ne is not equal to operator
