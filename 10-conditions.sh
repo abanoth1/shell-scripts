c128aa7c33e2b4fb313c0c8d5e75fc23d91067dc
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 19:23:46 -0600

    conditions added 1

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,7 +5,7 @@
 NUMBER=$1
 
 if [ "$NUMBER" -lt 10 ]; then   # -lt is less than operator
-    echo "The given $NUMBER is less than 10."
+    echo "The given number $NUMBER is less than 10."
 else
-    echo "The number $NUMBER is greater than or equal to 10."
+    echo "The given number $NUMBER is greater than or equal to 10."
 fi
