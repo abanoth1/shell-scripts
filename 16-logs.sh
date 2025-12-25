b6b019f05cd567d4e2fa97821c17199ad03e9ac8
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 23:31:02 -0600

    logs added

--- 16-logs.sh
+++ 16-logs.sh
@@ -12,10 +12,11 @@ N='\e[0m'  # NO COLOR
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$(date +%F-%H-%M-%S).log"
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 
 mkdir -p $LOGS_FOLDER
+echo "script execution started at : $(date)"
 
 USERID=$(id -u)
 if [ "$USERID" -ne 0 ]; then
