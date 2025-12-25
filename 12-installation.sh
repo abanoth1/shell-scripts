2e4e4e38ec2519107376c4522c8d48bf97144cc5
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 21:33:58 -0600

    installation

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,13 +4,18 @@
 USERID=$(id -u)
 if [ "$USERID" -ne 0 ]; then
     echo "Error: Please run this script as root user or using sudo."
+    exit 1 # failure is indicated by non-zero exit status
 
 fi
+# Here the shell script is executed even though the user is not root
+# because there is no exit command after the error message.
+# To fix this, we can add an exit command after the error message.
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo " Error: MySQL installation failed."
+    exit 1
 else
     echo " MySQL installation completed successfully."
 fi
