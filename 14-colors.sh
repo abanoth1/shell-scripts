861a37ae2e4828c8f0535a6b40802cacb96772a9
abanoth <ajaybanoth.aj@gmail.com>
2025-12-24 22:11:51 -0600

    colors

---
+++ 14-colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# WORKING WITH COLORS IN SHELL SCRIPTING
+
+R='\e[31m' # RED COLOR
+G='\e[32m' # GREEN COLOR
+Y='\e[33m' # YELLOW COLOR
+B='\e[34m' # BLUE COLOR
+M='\e[35m' # MAGENTA COLOR
+C='\e[36m' # CYAN COLOR
+W='\e[37m' # WHITE COLOR
+N='\e[0m'  # NO COLOR
+
+echo -e "$G Hello this is green color $N"
