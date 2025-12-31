00c05d7a8f4300313a0d2ef95bbd90298ce3e102
abanoth <ajaybanoth.aj@gmail.com>
2025-12-30 23:05:55 -0600

    set command added

--- 19-set.sh
+++ 19-set.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 # WORKING WITH COLORS IN SHELL SCRIPTING AND ADDING COLORS TO OUTPUT  AND ALSO USING FUNCTIONS AND LOOPS
+
+set -e # exit immediately if any command exits with a non-zero status
 echo "Hello .." 
 echo " Before the error..."
 ccaffj1;dnf
