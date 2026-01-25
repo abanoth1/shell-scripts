ed4ac2589479f2975de0b0f26e256e16ad5fea74
abanoth <ajaybanoth.aj@gmail.com>
2026-01-25 14:46:08 -0600

    woring on cpu usage

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -7,6 +7,15 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 while IFS= read -r line; 
 do
 
-  echo " Line: $line" # printing each line
+#   echo " Line: $line" # printing each line
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f 1) # extracting the usage percentage
+    PARTITION=$(echo $line | awk '{print $7}') # extracting the partition name
+    TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S') # getting the current timestamp
+
+    if [ $USAGE -ge $DISK_USAGE ]; then
+    echo "High disk usage on $PARTITION: $USAGE% at $TIMESTAMP"
+    # Here you can add code to send an alert email or notification
+    fi
+
 
 done <<< "$DISK_USAGE"
