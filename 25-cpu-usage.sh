2919638956c125cee7f7abdfad5da19025febf4d
abanoth <ajaybanoth.aj@gmail.com>
2026-01-25 14:49:31 -0600

    ADDED THE THERSLOD LIMIT TO THE DISK

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -3,18 +3,18 @@
 # IT CAN BE SCHEDULED TO RUN PERIODICALLY USING CRON JOBS TO AUTOMATE THE MONITORING PROCESS
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # Set threshold percentage for disk usage alert
 
 while IFS= read -r line; 
-do
 
+do
 #   echo " Line: $line" # printing each line
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f 1) # extracting the usage percentage
     PARTITION=$(echo $line | awk '{print $7}') # extracting the partition name
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S') # getting the current timestamp
-    if [ $USAGE -ge $DISK_USAGE ]; then
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
         echo "High disk usage on $PARTITION: $USAGE% at $TIMESTAMP"
     # Here you can add code to send an alert email or notification
     fi
-
 
 done <<< "$DISK_USAGE"
